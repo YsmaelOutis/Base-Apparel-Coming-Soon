8f3defc60446f4f1e5ddecc0201ad63c93d9ba9e
Juan Cruz Caceres <cacerescallieri@gmail.com>
2023-08-29 01:47:42 -0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [email, setEmail] = useState('')
+  const [error, setError] = useState(false)
+
+  function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/
+    return re.test(email)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const data  = new FormData(e.target)
+    const emailData = Object.fromEntries(data.entries()).email
+
+    if (!validateEmail(emailData)) {
+      setError(true)
+      setEmail('')
+    } else {
+      setError(false)
+    }
+  }
+
+  return (
+    <>
+      <div className="brand-mobile">
+        <img src="./Brand.jpg" alt="Base Apparel Branding" />
+      </div>
+
+      <main>
+        <article>
+          <div className="brand-desktop">
+            <img src="./Brand.jpg" alt="Base Apparel Branding" />
+          </div>
+
+          <h1 className='pink'>WE'RE</h1>
+          <h1>COMING<br></br>
+          SOON</h1>
+
+          <p>Hello fellow shoppers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launch deals.</p>
+
+          <form onSubmit={handleSubmit}>
+            <input type="email" placeholder='Email Address' className={error ? 'error' : ''} name={"email"}/>
+            {error && (
+              <>
+                <img src="./ErrorIcon.svg" alt="Error Icon" className='error-icon'/>
+                <p className='error-text'>Please provide a valid email</p>
+              </>
+            )}
+            <button type="submit" formNoValidate>{'>'}</button>
+          </form>
+        </article>
+
+        <img src="./Banner.jpg" alt="Girl posing with Base Apparel Clothing" className='girl-picture'/>
+      </main>
+    </>
+  )
+}
+
+export default App
